d7b69328d0e5e72a33de23e9dd9fc19011422e40
Luke Shay <shay.luke17@gmail.com>
2022-02-02 15:22:21 -0600

    chore: more deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,14 @@
 module.exports = {
   extends: ['get-off-my-lawn', 'plugin:sonarjs/recommended'],
+  overrides: [
+    {
+      files: ['test/**/*'],
+      rules: { '@typescript-eslint/unbound-method': 'off' },
+    },
+  ],
   plugins: ['sonarjs'],
   rules: {
+    'node/prefer-global/process': 'off',
     'unicorn/prefer-export-from': 'off',
   },
-  overrides: [{ files: ['test/**/*'], rules: { '@typescript-eslint/unbound-method': 'off' } }],
 };
